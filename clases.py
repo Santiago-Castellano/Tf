bd3719e27d6b7111b8ed23c531fb4c1cbcaf2896
Santaigo-Castellano <santiagocastellano01@gmail.com>
2021-02-21 19:42:55 -0300

    el que mas autos tenga

--- clases.py
+++ clases.py
@@ -27,33 +27,31 @@ class Interseccion():
         self.prioridad = prioridad
         self.tiempo_inactivo = 0
         self.vehiculos = 0
+        self.f = tf.gfile.FastGFile('frozen_inference_graph.pb', 'rb')
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(self.f.read())
+        self.sess = tf.Session()
+        self.sess.graph.as_default()
+        tf.import_graph_def(graph_def, name='')
 
     async def cantidad_vehiculos(self):
         while True:
             self._cantidad_vehiculos()
             print(f"contando vehiculos inter: {self.id} Total: {self.vehiculos}")
-            await asyncio.sleep(3)
+            await asyncio.sleep(6)
     
     def _cantidad_vehiculos(self):
-        with tf.gfile.FastGFile('frozen_inference_graph.pb', 'rb') as f:
-            graph_def = tf.GraphDef()
-            graph_def.ParseFromString(f.read())
-            
-        with tf.Session() as sess:
-            sess.graph.as_default()
-            tf.import_graph_def(graph_def, name='')
+        img = cv.imread(self.obtener_imagen())
+        inp = cv.resize(img, (300, 300))
+        inp = inp[:, :, [2, 1, 0]]  # BGR2RGB
 
-            img = cv.imread(self.obtener_imagen())
-            inp = cv.resize(img, (300, 300))
-            inp = inp[:, :, [2, 1, 0]]  # BGR2RGB
+        out = self.sess.run([self.sess.graph.get_tensor_by_name('num_detections:0'),
+                        self.sess.graph.get_tensor_by_name('detection_scores:0'),
+                        self.sess.graph.get_tensor_by_name('detection_boxes:0'),
+                        self.sess.graph.get_tensor_by_name('detection_classes:0')],
+                    feed_dict={'image_tensor:0': inp.reshape(1, inp.shape[0], inp.shape[1], 3)})
 
-            out = sess.run([sess.graph.get_tensor_by_name('num_detections:0'),
-                            sess.graph.get_tensor_by_name('detection_scores:0'),
-                            sess.graph.get_tensor_by_name('detection_boxes:0'),
-                            sess.graph.get_tensor_by_name('detection_classes:0')],
-                        feed_dict={'image_tensor:0': inp.reshape(1, inp.shape[0], inp.shape[1], 3)})
-
-            num_detections = int(out[0][0])
+        num_detections = int(out[0][0])
 
         self.vehiculos = num_detections
 
@@ -78,10 +76,15 @@ class Manejador():
         Metodo encargado de la logica para seleccionar que interseccion continuara.
         """
         print("seleccionando interseccion")
-        self.activo = random.choice(self.intersecciones)
+        inter_max = self.intersecciones[0]
+        for inter in self.intersecciones:
+            if inter_max.vehiculos < inter.vehiculos:
+                inter_max = inter
+
+        self.activo = inter_max
 
     async def activar_semaforo(self):
-        print(f"se activara el semaforo {self.activo.id}.-\n Tiempo inactivo: {self.activo.tiempo_inactivo}")
+        print(f"se activara el semaforo {self.activo.id}-\nPor {self.activo.tiempo_activacion} s. -\n Tiempo inactivo: {self.activo.tiempo_inactivo}")
         self.activo.tiempo_inactivo = 0
         for inter in self.intersecciones:
             if inter.id != self.activo.id:
